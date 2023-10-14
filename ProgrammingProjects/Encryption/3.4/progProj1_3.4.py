74026f109af29f44717849179642ccaabf43301b
Seth Weiss <sweisss@gmail.com>
2023-10-14 11:54:33 -0700

    Add example encoding

--- ProgrammingProjects/Encryption/3.4/progProj1_3.4.py
+++ ProgrammingProjects/Encryption/3.4/progProj1_3.4.py
@@ -17,6 +17,7 @@ bits. Your goal is to write a program to find out this key.
 Cited sources:
 https://canvas.oregonstate.edu/courses/1933665/pages/programming-project-encryption-helper-3-dot-4?wrap=1
 https://pycryptodome.readthedocs.io/en/latest/src/cipher/classic.html#cbc-mode
+https://stackoverflow.com/questions/7585435/best-way-to-convert-string-to-bytes-in-python-3
 """
 import sys
 
@@ -29,60 +30,86 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
 
-data = b"secret"
-key = get_random_bytes(16)
-cipher = AES.new(key, AES.MODE_CBC)
-ct_bytes = cipher.encrypt(pad(data, AES.block_size))
-iv = b64encode(cipher.iv).decode('utf-8')
-ct = b64encode(ct_bytes).decode('utf-8')
-result = json.dumps({'iv':iv, 'ciphertext':ct})
-print(result)
+# data = b"secret"
+# # key = get_random_bytes(16)
+# key = pad(b'key1', AES.block_size)
+# print(f'Key: {key}')
+# cipher = AES.new(key, AES.MODE_CBC)
+# ct_bytes = cipher.encrypt(pad(data, AES.block_size))
+# iv = b64encode(cipher.iv).decode('utf-8')
+# ct = b64encode(ct_bytes).decode('utf-8')
+# result = json.dumps({'iv':iv, 'ciphertext':ct})
+# print(result)
 
 
-try:
-    b64 = json.loads(result)
-    iv = b64decode(b64['iv'])
-    ct = b64decode(b64['ciphertext'])
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    pt = unpad(cipher.decrypt(ct), AES.block_size)
-    print("The message was: ", pt)
-except (ValueError, KeyError):
-    print("Incorrect decryption")
+# try:
+#     b64 = json.loads(result)
+#     iv = b64decode(b64['iv'])
+#     ct = b64decode(b64['ciphertext'])
+#     cipher = AES.new(key, AES.MODE_CBC, iv)
+#     pt = unpad(cipher.decrypt(ct), AES.block_size)
+#     print("The message was: ", pt)
+# except (ValueError, KeyError):
+#     print("Incorrect decryption")
 
-
-
+PLAINTEXT = "This is a top secret."
+CIPHER_STR = "8d20e5056a8d24d0462ce74e4904c1b513e10d1df4a2ef2ad4540fae1ca0aaf9"
+CIPHER_HEX = bytearray.fromhex(CIPHER_STR)
+MAX_KEY_LEN = 15
 DEBUG = True
 
+debug_print = lambda input: print(input) if DEBUG else 0
 
 
-key = get_random_bytes(16)
-cipher = AES.new(key, AES.MODE_CBC)
-print(cipher)
+# key = get_random_bytes(16)
+# cipher = AES.new(key, AES.MODE_CBC)
 
 
-# plaintext1 = 'This is a top secret.'
-# print(f'paintext1: {plaintext1}') if DEBUG else 0
+# plaintext1 = b'This is a top secret.'
+# debug_print(f'paintext1: {plaintext1}')
 
-# ciphertextHexValuesAsString = "8d20e5056a8d24d0462ce74e4904c1b513e10d1df4a2ef2ad4540fae1ca0aaf9"
-# print('Ciphertext: "', ciphertextHexValuesAsString, '"', sep="") if DEBUG else 0
-# print("Data Type: ", type(ciphertextHexValuesAsString)) if DEBUG else 0
-
-# print("Converting String to bytes...") if DEBUG else 0
-# ciphertextHexValuesAsBytes = bytearray.fromhex(ciphertextHexValuesAsString)
-
-# print('Ciphertext: "' + ''.join(format(x, '02x') for x in ciphertextHexValuesAsBytes) + '"') if DEBUG else 0
+# text = 'text example'
+# print(text)
+# text2 = text.encode('utf-8')
+# print(text2)
 
 
 
-# def main():
-#     param_count = len(sys.argv)
+def load_key_words(file: str) -> list:
+    with open(file, 'r') as f:
+        words = f.readlines()
 
-#     print(f'Num params: {param_count}') if DEBUG else 0
-#     print(f'sys.argv[0]: {sys.argv[0]}') if DEBUG else 0
-
-#     print(f'sys.argv[1]: {sys.argv[1]}') if (DEBUG and param_count > 1) else 0
-#     print(f'sys.argv[2]: {sys.argv[2]}') if (DEBUG and param_count > 2) else 0
+    return words
 
 
-# if __name__ == "__main__":
-#     main()
+def filter_words(words: list, wordlen: int) -> list:
+    small_words = [word.strip() for word in words if len(word) <= wordlen]
+    debug_print(f'Words in words.txt: {len(words)}')
+    debug_print(f'Words less than 16 chars in words.txt: {len(small_words)}')
+
+    return small_words
+
+
+def main():
+    words = load_key_words('./words.txt')
+    small_words = filter_words(words, MAX_KEY_LEN)
+
+    first_word = small_words[0]
+    debug_print(f'First word: {first_word}')
+    first_word_b = first_word.encode('utf-8')
+    debug_print(f'First word as bytes: {first_word_b}')    
+
+
+    plaintext_b = PLAINTEXT.encode('utf-8')
+    key = pad(first_word_b, AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC)
+    ct_bytes = cipher.encrypt(pad(plaintext_b, AES.block_size))
+    iv = b64encode(cipher.iv).decode('utf-8')
+    ct = b64encode(ct_bytes).decode('utf-8')
+    result = json.dumps({'iv':iv, 'ciphertext':ct})
+    print(result)
+
+
+
+if __name__ == "__main__":
+    main()
