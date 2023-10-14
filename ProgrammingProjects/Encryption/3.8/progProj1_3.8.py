b9f13e1b558772ca334803c6d4ab55482d4e3b8b
Seth Weiss <sweisss@gmail.com>
2023-10-14 14:39:56 -0700

    Start progProj1 3.8

---
+++ ProgrammingProjects/Encryption/3.8/progProj1_3.8.py
@@ -0,0 +1,37 @@
+"""
+Seth Weiss
+weissse@oregonstate.edu
+CS 370 - Intro to Security
+Fall 2023
+
+Description:
+In this task, we will investigate the difference between a hash function's two properties: weak 
+collision resistance property versus strong collision-resistance property. You will use the brute-
+force method to see how long it takes to break each of these properties. Instead of using 
+OpenSSL's command-line tools, you are required to write your own programs to invoke the message 
+digest functions in OpenSSL's crypto library.
+
+To make the task feasible, we reduce the length of the hash value to 24 bits. We can use any one-way hash 
+function, but we only use the first 24 bits of the hash value in this task. Namely, we are using a 
+modified one-way hash function.
+
+- How many trials will it take to break the weak collision resistance property using 
+the brute-force method? You should repeat your experiment for multiple times (100 or more 
+depending on how long each trial takes), and report your average number of trials.  
+
+- How many trials will it take you to break the collision-free property using the brute-
+force method? Similarly, you should report the average.  
+
+- Based on your observation, which property is easier to break using the brute-force 
+method?  
+
+- Can you explain the difference in your observations?
+
+Resources:
+https://pycryptodome.readthedocs.io/en/latest/src/hash/hash.html#
+"""
+from Crypto.Hash import SHA256
+
+
+hash_object = SHA256.new(data=b'First')
+print(type(hash_object))
