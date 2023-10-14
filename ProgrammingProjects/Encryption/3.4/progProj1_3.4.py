416aaf25222b742f9079a6b818cc3b4843a9c550
Seth Weiss <sweisss@gmail.com>
2023-10-14 09:59:43 -0700

    Fix ' in program description

--- ProgrammingProjects/Encryption/3.4/progProj1_3.4.py
+++ ProgrammingProjects/Encryption/3.4/progProj1_3.4.py
@@ -8,7 +8,7 @@ Programming Project 1 - 3.4
 Description:
 You are given a plaintext and a ciphertext, and you know that aes-128-cbc is used to generate 
 the ciphertext from the plaintext, and you also know that the numbers in the IV are all zeros 
-(not the ASCII character ‘0’). Another clue that you have learned is that the key used to encrypt 
+(not the ASCII character '0'). Another clue that you have learned is that the key used to encrypt 
 this plaintext is an English word shorter than 16 characters; the word that can be found from a 
 typical English dictionary. Since the word has less than 16 characters (i.e. 128 bits), space 
 characters (hexadecimal value 0x20) are appended to the end of the word to form a key of 128 
