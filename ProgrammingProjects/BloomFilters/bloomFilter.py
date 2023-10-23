93375227e0616759f921d603c8f1c67b3bc5f00c
Seth Weiss <sweisss@gmail.com>
2023-10-22 21:03:12 -0700

    Start bloomFilter.py Add program heading

---
+++ ProgrammingProjects/BloomFilters/bloomFilter.py
@@ -0,0 +1,27 @@
+"""
+Seth Weiss
+weissse@oregonstate.edu
+CS 370 - Intro to Security
+Fall 2023
+Programming Project 2  - Bloom Filters
+
+Project Requirements:
+- The software will implement a Bloom filter.
+- Bloom filter will be loaded with values from rockyou.txt.
+- The software will automate the testing of values in dictionary.txt.
+- The software will calculate and display statistics on true positive, 
+    true negative, false positive, and false negative for the dictionary.txt 
+    based on the rockyou.txt.
+
+Design Considerations:
+- Decide how big of a bit array you will use and understand the impact
+    of such a decision.
+- Decide how many hashing algorithms you will use and understand the impact
+    of such a decision.
+- Understand the difference between true positive, true negative, false positive,
+    and false negative in relation to the results of a Bloom Filter.
+- Check out the runtime difference between using a List and a SortedSet when you are
+    comparing which values in dictionary.txt are in rockyou.txt (re: reference statistics).
+"""
+
+
